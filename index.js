bf3e6dff5250c3c6087e51e7dfbeb7e1979f0aeb
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-12 22:47:15 +0500

    javascript assignment 09 on working

---
+++ index.js
@@ -0,0 +1,9 @@
+// QUESTION 01) Write a program that takes two user inputs for first and
+// last name using prompt and merge them in a new variable
+// titled fullName. Greet the user using his full name.
+
+let firstName = prompt("enter your first name");
+let secondName = prompt("enter your secong name");
+
+let fullName = `Assalam o Alaikum, Good Morning ${firstName} ${secondName}`
+alert(fullName)
