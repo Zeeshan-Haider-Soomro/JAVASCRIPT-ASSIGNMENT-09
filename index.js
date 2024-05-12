d13e3df4acea34bd2c2347efa2016e3b9df219df
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-12 23:18:34 +0500

    on process

--- index.js
+++ index.js
@@ -2,8 +2,50 @@
 // last name using prompt and merge them in a new variable
 // titled fullName. Greet the user using his full name.
 
-let firstName = prompt("enter your first name");
-let secondName = prompt("enter your secong name");
+// let firstName = prompt("enter your first name");
+// let secondName = prompt("enter your secong name");
 
-let fullName = `Assalam o Alaikum, Good Morning ${firstName} ${secondName}`
-alert(fullName)
+// let fullName = `Assalam o Alaikum, Good Morning ${firstName} ${secondName}`
+// alert(fullName)
+
+// QUSTION 02) Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+
+// let favoriteMobilephone = prompt("enter your favorite mobile phone model");
+
+// let lengthNum = favoriteMobilephone.length
+
+// document.write(`My favorite phone is: ${favoriteMobilephone} <br/> Length of string: ${lengthNum}`)
+
+// QUESTION 03)Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser
+
+// let country = "Pakistani"
+// let findIndex = country.indexOf("n")
+// document.write(`String: ${country} <br/> Indexof 'n': ${findIndex}`)
+
+// QUESTION 04)Write a program to find the last index of letter “l” in the
+// word “Hello World” and display the result in your browser.
+
+// let greet = "Hello World";
+// let findLastindexOf = greet.lastIndexOf("l");
+// document.write(`String: ${greet} <br/> Last indexOf 'l' ${findLastindexOf}`)
+
+// QUESTION 05)Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+
+// let country = "Pakistani";
+// let findIndexOf = country.charAt(3);
+// document.write(`String: ${country} <br/> Character at index 3: ${findIndexOf}`)
+
+
+// QUESTION 06) Repeat Q1 using string concat() method.
+
+// let firstName = prompt("enter your first name");
+// let secondName = prompt("enter your secong name");
+// let fullName = "Good Morning" + " " + firstName + " " + secondName
+// alert(fullName)
+
+
+
