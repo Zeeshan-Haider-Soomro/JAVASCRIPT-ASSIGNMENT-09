2c5e03d60fbec2545dafd6fc92019633b7202ca9
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-13 19:42:08 +0500

    chapter 21-25

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-// QUESTION 01) Write a program that takes two user inputs for first and
-// last name using prompt and merge them in a new variable
-// titled fullName. Greet the user using his full name.
+// QUESTION 01) Write a program that takes two user inputs for first and last name using prompt and merge them in a new variable titled fullName. Greet the user using his full name.
 
 // let firstName = prompt("enter your first name");
 // let secondName = prompt("enter your secong name");
-
 // let fullName = `Assalam o Alaikum, Good Morning ${firstName} ${secondName}`
 // alert(fullName)
 
-// QUSTION 02) Write a program to take a user input about his favorite
-// mobile phone model. Find and display the length of user
-// input in your browser
+// QUSTION 02) Write a program to take a user input about his favorite mobile phone model. Find and display the length of user input in your browser
 
 // let favoriteMobilephone = prompt("enter your favorite mobile phone model");
-
 // let lengthNum = favoriteMobilephone.length
-
 // document.write(`My favorite phone is: ${favoriteMobilephone} <br/> Length of string: ${lengthNum}`)
 
-// QUESTION 03)Write a program to find the index of letter “n” in the word
-// “Pakistani” and display the result in your browser
+// QUESTION 03)Write a program to find the index of letter “n” in the word “Pakistani” and display the result in your browser
 
 // let country = "Pakistani"
 // let findIndex = country.indexOf("n")
 // document.write(`String: ${country} <br/> Indexof 'n': ${findIndex}`)
 
-// QUESTION 04)Write a program to find the last index of letter “l” in the
-// word “Hello World” and display the result in your browser.
+// QUESTION 04)Write a program to find the last index of letter “l” in the word “Hello World” and display the result in your browser.
 
 // let greet = "Hello World";
 // let findLastindexOf = greet.lastIndexOf("l");
 // document.write(`String: ${greet} <br/> Last indexOf 'l' ${findLastindexOf}`)
 
-// QUESTION 05)Write a program to find the character at 3rd index in the
-// word “Pakistani” and display the result in your browser.
+// QUESTION 05)Write a program to find the character at 3rd index in the word “Pakistani” and display the result in your browser.
 
 // let country = "Pakistani";
 // let findIndexOf = country.charAt(3);
@@ -44,8 +34,46 @@
 
 // let firstName = prompt("enter your first name");
 // let secondName = prompt("enter your secong name");
-// let fullName = "Good Morning" + " " + firstName + " " + secondName
-// alert(fullName)
+// alert(`Good morning ${firstName.concat(secondName)}`)
+
+// QUESTION 07)Write a program to replace the “Hyder” to “Islam” in the word “Hyderabad” and display the result in your browser.
+
+// let city = "hyderabad";
+// document.write(`City: ${city} <br/> after replacment: ${city.replace("hyder", "islam")}`)
+
+// QUESTION 08)Write a program to replace all occurrences of “and” in the string with “&” and display the result in your browser.
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// document.write(`${message.replaceAll("and","&")}`)
+
+// QUESTION 09)Write a program that converts a string “472” to a number 472. Display the values & types in your browser.
+
+// let convert = "472";
+// document.write(`Value: ${convert} <br/> TypeOf: ${typeof convert} <br/> Value: ${+convert} <br/> TypeOf: ${typeof +convert}`)
+
+// QUESTION 10)Write a program that takes user input. Convert and show the input in capital letters.
+
+// let convertInUppercase = prompt("Enter a word to captilize it");
+// document.write(`User input: ${convertInUppercase} <br/> Upper case: ${convertInUppercase.toUpperCase()}`)
+
+// QUESTION 11)Write a program that takes user input. Convert and show the input in title case.
+
+// let convertInUppercase = prompt("Enter a word to captilize it");
+// document.write(`User input: ${convertInUppercase} <br/> Title case: ${convertInUppercase.charAt(0).toUpperCase() + convertInUppercase.slice(1)}`)
+
+
+// QUESTION 12)Write a program that converts the variable num to string.
+
+// var num = 35.36 ;
+// document.write(`Number: ${num} <br/> Result: ${num.toString().replace(".", "")}`)
+
+// QUESTION 13)
+
+
+
+
+
+
 
 
 
